a4a23ccb4865969e23959de96c3354c85db61780
combarim <combarim2@gmail.com>
2025-12-04 21:19:28 +0100

    Add an initializer to create initials tasks

--- src/main/java/marius/ma/todoappandgithubactions/TodoAppAndGithubActionsApplication.java
+++ src/main/java/marius/ma/todoappandgithubactions/TodoAppAndGithubActionsApplication.java
@@ -1,7 +1,11 @@
 package marius.ma.todoappandgithubactions;
 
+import marius.ma.todoappandgithubactions.dto.TaskDto;
+import marius.ma.todoappandgithubactions.service.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoAppAndGithubActionsApplication {
@@ -10,4 +14,12 @@ public class TodoAppAndGithubActionsApplication {
         SpringApplication.run(TodoAppAndGithubActionsApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(TaskService taskService) {
+        return args -> {
+            taskService.createTask(new TaskDto(null, "Learn Spring Boot", "Study Spring Boot framework", false));
+            taskService.createTask(new TaskDto(null, "Build REST API", "Develop a RESTful API for tasks", false));
+            taskService.createTask(new TaskDto(null, "Deploy to Production", "Deploy the application to a production environment", false));
+        };
+    }
 }
